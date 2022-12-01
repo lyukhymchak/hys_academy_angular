46cdee83c03e88cea4a6d540aa8476062aedce3b
Liana <l.yukhymchak@gmail.com>
2022-12-01 22:34:37 +0100

    ls_14 create html template for page, add static data for product cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
 
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'hys_academy_angular';
+  products: Array<Product> = [
+    {
+      id: 1,
+      name: 'Bread',
+      price: 1,
+    },
+    {
+      id: 2,
+      name: 'Milk',
+      price: 2,
+    },
+    {
+      id: 3,
+      name: 'Sugar',
+      price: 1,
+    },
+    {
+      id: 4,
+      name: 'Salt',
+      price: 2,
+    },
+    {
+      id: 5,
+      name: 'Tea',
+      price: 1,
+    },
+    {
+      id: 6,
+      name: 'Salt',
+      price: 2,
+    },
+    {
+      id: 7,
+      name: 'Tea',
+      price: 1,
+    },
+    {
+      id: 8,
+      name: 'Salt',
+      price: 2,
+    },
+  ];
 }
