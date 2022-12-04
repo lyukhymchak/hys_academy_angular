dde32775c8edd22669893fd727ee75f07d7a348a
Liana <l.yukhymchak@gmail.com>
2022-12-05 00:00:55 +0100

    ls_14 create data generation function, fix display of card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,46 +12,51 @@ interface Product {
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  products: Array<Product> = [
-    {
-      id: 1,
-      name: 'Bread',
-      price: 1,
-    },
-    {
-      id: 2,
-      name: 'Milk',
-      price: 2,
-    },
-    {
-      id: 3,
-      name: 'Sugar',
-      price: 1,
-    },
-    {
-      id: 4,
-      name: 'Salt',
-      price: 2,
-    },
-    {
-      id: 5,
-      name: 'Tea',
-      price: 1,
-    },
-    {
-      id: 6,
-      name: 'Salt',
-      price: 2,
-    },
-    {
-      id: 7,
-      name: 'Tea',
-      price: 1,
-    },
-    {
-      id: 8,
-      name: 'Salt',
-      price: 2,
-    },
-  ];
+  products: Array<Product> = this.getProducts();
+
+  getProducts(count: number = 8): Array<Product> {
+    const producers: string[] = [
+      'Starbucks',
+      'Nespresso',
+      'Jacobs',
+      'Lavazza',
+      'Costa Coffee ',
+      'Illy',
+      'Kimbo',
+      'Movenpick',
+      'Tchibo',
+      'Odeska kava',
+    ];
+
+    const typeOfDrink: string[] = [
+      'Espresso',
+      'Double Espresso',
+      'Americano',
+      'Cappuccino',
+      'Flat White',
+      'Cafe Latte',
+      'Vienna',
+      'Mocha',
+      'Macchiato',
+      'Iced Coffee',
+    ];
+
+    const data = new Array<Product>();
+
+    for (let i = 0; i < count; i++) {
+      const productName: string =
+        typeOfDrink[this.getRandomInteger(typeOfDrink.length)] +
+        ' ' +
+        producers[this.getRandomInteger(producers.length)];
+      const productPrice: number = 2 + this.getRandomInteger(5);
+
+      data.push({ id: i + 1, name: productName, price: productPrice });
+    }
+
+    return data;
+  }
+
+  getRandomInteger(max: number): number {
+    return Math.floor(Math.random() * max);
+  }
 }
