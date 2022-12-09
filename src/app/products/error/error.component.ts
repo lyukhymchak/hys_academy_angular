db4896183237a8ce7e25f35d23f70a101137d3ff
Liana <l.yukhymchak@gmail.com>
2022-12-10 00:36:33 +0100

    ls_16 add click event to button in error component

--- src/app/products/error/error.component.ts
+++ src/app/products/error/error.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html',
   styleUrls: ['./error.component.scss'],
 })
-export class ErrorComponent {
+export class ErrorComponent implements OnInit {
   buttonClassNames: string[] = ['button', 'btn-load'];
+
+  constructor(private route: Router) {}
+  ngOnInit(): void {}
+
+  backToHome() {
+    this.route.navigate(['products']);
+  }
 }
