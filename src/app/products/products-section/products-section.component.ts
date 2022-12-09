b0789c839bad6742683b3fdd1438f0fcb90323f2
Liana <l.yukhymchak@gmail.com>
2022-12-09 03:18:47 +0100

    ls_15 create service

--- src/app/products/products-section/products-section.component.ts
+++ src/app/products/products-section/products-section.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import Product from '../interfaces/product.interface';
+import { ProductsService } from '../products.service';
 
 @Component({
   selector: 'app-products-section',
@@ -10,55 +11,9 @@ export class ProductsSectionComponent implements OnInit {
   products: Array<Product>;
   buttonClassNames: string[] = ['button', 'btn-load'];
 
-  constructor() {}
+  constructor(private productsService: ProductsService) {}
 
   ngOnInit(): void {
-    this.products = this.getProducts();
-  }
-
-  getProducts(count: number = 8): Array<Product> {
-    const producers: string[] = [
-      'Starbucks',
-      'Nespresso',
-      'Jacobs',
-      'Lavazza',
-      'Costa Coffee ',
-      'Illy',
-      'Kimbo',
-      'Movenpick',
-      'Tchibo',
-      'Odeska kava',
-    ];
-
-    const typeOfDrink: string[] = [
-      'Espresso',
-      'Double Espresso',
-      'Americano',
-      'Cappuccino',
-      'Flat White',
-      'Cafe Latte',
-      'Vienna',
-      'Mocha',
-      'Macchiato',
-      'Iced Coffee',
-    ];
-
-    const data = new Array<Product>();
-
-    for (let i = 0; i < count; i++) {
-      const productName: string =
-        typeOfDrink[this.getRandomInteger(typeOfDrink.length)] +
-        ' ' +
-        producers[this.getRandomInteger(producers.length)];
-      const productPrice: number = 2 + this.getRandomInteger(5);
-
-      data.push({ id: i + 1, name: productName, price: productPrice });
-    }
-
-    return data;
-  }
-
-  getRandomInteger(max: number): number {
-    return Math.floor(Math.random() * max);
+    this.products = this.productsService.getProducts();
   }
 }
