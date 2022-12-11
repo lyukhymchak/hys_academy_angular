c74273e07807e6a1551a16a92f0ee32d09f2f013
Liana <33780508+lyukhymchak@users.noreply.github.com>
2022-12-11 15:33:48 +0100

    Ls 14 (#1)
    * ls_14 add fonts
    
    * ls_14 create html template for page, add static data for product cards
    
    * ls_14 add styles
    
    * ls_14 create data generation function, fix display of card
    
    * ls_14 move  scss variables in separate file
    
    * ls_14 add transition property to some classes, change logo and favicon.ico
    
    * ls_14 add normalize.css as devDependency to package json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,62 @@
 import { Component } from '@angular/core';
 
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'hys_academy_angular';
+  products: Array<Product> = this.getProducts();
+
+  getProducts(count: number = 8): Array<Product> {
+    const producers: string[] = [
+      'Starbucks',
+      'Nespresso',
+      'Jacobs',
+      'Lavazza',
+      'Costa Coffee ',
+      'Illy',
+      'Kimbo',
+      'Movenpick',
+      'Tchibo',
+      'Odeska kava',
+    ];
+
+    const typeOfDrink: string[] = [
+      'Espresso',
+      'Double Espresso',
+      'Americano',
+      'Cappuccino',
+      'Flat White',
+      'Cafe Latte',
+      'Vienna',
+      'Mocha',
+      'Macchiato',
+      'Iced Coffee',
+    ];
+
+    const data = new Array<Product>();
+
+    for (let i = 0; i < count; i++) {
+      const productName: string =
+        typeOfDrink[this.getRandomInteger(typeOfDrink.length)] +
+        ' ' +
+        producers[this.getRandomInteger(producers.length)];
+      const productPrice: number = 2 + this.getRandomInteger(5);
+
+      data.push({ id: i + 1, name: productName, price: productPrice });
+    }
+
+    return data;
+  }
+
+  getRandomInteger(max: number): number {
+    return Math.floor(Math.random() * max);
+  }
 }
