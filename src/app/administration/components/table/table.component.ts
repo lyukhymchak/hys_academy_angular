4133e9b8f4af5f3695b09b8245afb3020ccd16f0
Liana <l.yukhymchak@gmail.com>
2022-12-24 01:30:41 +0100

    ls_19 add sorting to the table component, update styling and positioning inside table

--- src/app/administration/components/table/table.component.ts
+++ src/app/administration/components/table/table.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { delay, Subject, of, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-table',
@@ -7,13 +6,51 @@ import { delay, Subject, of, Observable } from 'rxjs';
   styleUrls: ['./table.component.scss'],
 })
 export class TableComponent implements OnInit {
-  @Input() public items: any;
+  @Input() public items: Array<any>;
   public keys: string[];
+  public data: Array<any>;
+  public sortKey: string;
+  public sortDirection: 'asc' | 'desc' = 'asc';
 
   constructor() {}
 
   ngOnInit(): void {
-    this.keys = Object.keys(this.items[0]);
+    if (this.items.length) {
+      this.keys = Object.keys(this.items[0]);
+      this.data = [...this.items];
+    }
+  }
+
+  sort(key: string) {
+    if (key === this.sortKey) {
+      this.sortDirection = this.sortDirection === 'asc' ? 'desc' : 'asc';
+    } else {
+      this.sortKey = key;
+      this.sortDirection = 'asc';
+    }
+    this.data.sort((a, b) => {
+      const fieldA = a[key];
+      const fieldB = b[key];
+      const type = typeof fieldA;
+      switch (type) {
+        case 'number':
+          return (fieldA - fieldB) * (this.sortDirection === 'asc' ? 1 : -1);
+        case 'string':
+          return (
+            fieldA.localeCompare(fieldB) *
+            (this.sortDirection === 'asc' ? 1 : -1)
+          );
+        case 'object':
+          if (fieldA instanceof Date) {
+            return (
+              (fieldA.getTime() - fieldB.getTime()) *
+              (this.sortDirection === 'asc' ? 1 : -1)
+            );
+          }
+          break;
+      }
+      return -1;
+    });
   }
 
   public capitalizeFirstLetter(word: string): string {
