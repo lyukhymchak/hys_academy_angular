6812c64909238c75ab100faf436cb46836e7938e
Liana <l.yukhymchak@gmail.com>
2022-12-27 21:58:30 +0100

    ls_19 add sorting to table component

--- src/app/shared/components/table/table.component.ts
+++ src/app/shared/components/table/table.component.ts
@@ -1,18 +1,68 @@
 import { Component, Input, OnInit } from '@angular/core';
 
+enum SortDirection {
+  Ascending = 'asc',
+  Descending = 'desc',
+}
+
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss'],
 })
 export class TableComponent implements OnInit {
-  @Input() public items: any;
+  @Input() public items: Array<any>;
   public keys: string[];
+  public data: Array<any>;
+  public sortKey: string;
+  public sortDirection: SortDirection = SortDirection.Ascending;
 
   constructor() {}
 
   ngOnInit(): void {
-    this.keys = Object.keys(this.items[0]);
+    if (this.items.length) {
+      this.keys = Object.keys(this.items[0]);
+      this.data = [...this.items];
+      this.sort(this.keys[0]);
+    }
+  }
+
+  public sort(key: string): void {
+    if (key === this.sortKey) {
+      this.sortDirection =
+        this.sortDirection === SortDirection.Ascending
+          ? SortDirection.Descending
+          : SortDirection.Ascending;
+    } else {
+      this.sortKey = key;
+      this.sortDirection = SortDirection.Ascending;
+    }
+    this.data.sort((a, b) => {
+      const fieldA = a[key];
+      const fieldB = b[key];
+      const type = typeof fieldA;
+      switch (type) {
+        case 'number':
+          return (
+            (fieldA - fieldB) *
+            (this.sortDirection === SortDirection.Ascending ? 1 : -1)
+          );
+        case 'string':
+          return (
+            fieldA.localeCompare(fieldB) *
+            (this.sortDirection === SortDirection.Ascending ? 1 : -1)
+          );
+        case 'object':
+          if (fieldA instanceof Date) {
+            return (
+              (fieldA.getTime() - fieldB.getTime()) *
+              (this.sortDirection === SortDirection.Ascending ? 1 : -1)
+            );
+          }
+          break;
+      }
+      return -1;
+    });
   }
 
   public capitalizeFirstLetter(word: string): string {
